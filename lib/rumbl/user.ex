6e730d8c5b6981b0011f2ba6749c78a7a72a21cd
Ralph von der Heyden <ralph@rvdh.de>
2017-03-13 16:22:59 +0100

    add new/create users actions

--- lib/rumbl/user.ex
+++ lib/rumbl/user.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.User do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "users" do
     field :name, :string
@@ -8,5 +9,11 @@ defmodule Rumbl.User do
     field :password_hash, :string
 
     timestamps()
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
   end
 end
